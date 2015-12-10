21e0d1f29883e8a8690749aa0f7a78a14a7bac27
shivaprasad167 <shiva_prasad167@yahoo.co.in>
2015-12-10 23:13:27 +0530

    session_2_assignment_3

---
+++ numbers1/src/numbers1/numbers1.java
@@ -0,0 +1,29 @@
+package numbers1;
+
+public class numbers1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("increasing start");
+		for(int a =1;a<=5;a++)
+		{
+			for(int b =1;b<=a;b++)
+			{
+				System.out.println(b);
+			}
+			System.out.println();
+		}
+		System.out.println("decreasing starts");
+		for(int x =4;x>=1;x--)
+		{
+			for(int y =1;y<=x;y++)
+			{
+				/*System.out.println("hello");*/
+				System.out.println(y);
+			}
+			System.out.println();
+		}
+		System.out.println("End of the program");
+	}
+
+}
