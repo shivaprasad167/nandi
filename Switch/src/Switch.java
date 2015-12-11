2ce711690e19791ac5d4bdcf35d39d9a71ba94f1
shivaprasad167 <shiva_prasad167@yahoo.co.in>
2015-12-11 21:59:55 +0530

    Session2_assignment4

---
+++ Switch/src/Switch.java
@@ -0,0 +1,24 @@
+
+public class Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int month = 17;
+        switch (month) {
+            case 1:  System.out.println("31"); break;
+            case 2:  System.out.println("28"); break;
+            case 3:  System.out.println("31"); break;
+            case 4:  System.out.println("30"); break;
+            case 5:  System.out.println("31"); break;
+            case 6:  System.out.println("30"); break;
+            case 7:  System.out.println("31"); break;
+            case 8:  System.out.println("31"); break;
+            case 9:  System.out.println("30"); break;
+            case 10: System.out.println("31"); break;
+            case 11: System.out.println("30"); break;
+            case 12: System.out.println("31"); break;
+            default: System.out.println("Invalid month.");
+            break;
+        			}
+	}
+}
